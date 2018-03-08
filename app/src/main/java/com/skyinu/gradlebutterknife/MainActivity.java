091c9abb804803a75ddead3ea1c732d42781bc05
getletcodes <1062860950@qq.com>
2018-03-08 22:01:32 +0800

    init

--- app/src/main/java/com/skyinu/gradlebutterknife/MainActivity.java
+++ app/src/main/java/com/skyinu/gradlebutterknife/MainActivity.java
@@ -2,8 +2,14 @@ package com.skyinu.gradlebutterknife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.skyinu.annotations.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
